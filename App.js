a45bf2f93587103555bde12cfc4b6e893f835c16
SamiraAhmed1 <158283580+SamiraAhmed1@users.noreply.github.com>
2024-10-04 17:21:06 +0100

    App.js

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput, Button, Alert} from 'react-native';
+import Logo from './components/Logo';
 import React, {useState} from 'react';
 
+// You can import supported modules from npm
 
-export default function App(){
-  const [fullname, setFullname] = useState("Eoin");
-  return(
-    <View> 
-      <Text>Hello, World {fullname}</Text>
-      <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
-    </View>
-  )
-};
+export default function App() {
 
 
+  function buttonClicked() {
+    Alert.alert("Hello"+ "" + fname +"" + lname + "" + dob);
+
+}
+const [fname, setFname] = useState("Joe");
+const [lname, setLname] = useState("Bloggs");
+const [dob, setDob] = useState("22 August 1990");
+
+
+return (
+  <View>
+     <Logo/>
+  <TextInput placeholder= "Enter your firstname" onChangeText={(val) => setFname(val)}/>
+  <TextInput placeholder= "Enter your lastname" onChangeText={(val) => setLname(val)}/>
+  <TextInput placeholder= "Enter your date of birth" onChangeText={(val) => setDob(val)}/>
+
+  <Text>Hello {fname} {lname}. You were born on {dob}</Text>
+
+  <Button title= "SUBMIT" onPress={buttonClicked}/>
+
+
+     </View>
+   )
+  }
 
